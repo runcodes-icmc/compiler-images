9342956639658f2b23e6c815f99ab7785c323f46
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2023-09-12 21:43:32 -0300

    Revert "go back to octave 4.2.1 while trying to fix octave 8"
    This reverts commit ccd5643cb0f6bdffcca2d25ce6eeaae4db58d58b.

--- octave/octave-script.sh
+++ octave/octave-script.sh
@@ -1,13 +1,8 @@
-fix_warnings() {
-    test_id=$1
-    cp ../${test_id}.error ../tmp
-    sed 1,2d <../tmp >../${test_id}.error
-    rm ../tmp
-}
-
-monitor_max_ms=0
-
-run_command="octave-cli -H -W -q ${src_file}"
+if [ ! -f src/?akefile ]
+then
+    compilation_command=""
+    run_command="octave-cli -H -W -q ${src_file}"
+fi
 
 export OPENBLAS_NUM_THREADS=1
-run_tests "$run_command" '' fix_warnings
+run_tests "$run_command"
