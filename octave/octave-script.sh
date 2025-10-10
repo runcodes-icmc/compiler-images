78b2205f6a3fd8c4400c71a6845e40a09b784146
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-10-09 22:53:05 -0300

    update octave maximum mesmory size

--- octave/octave-script.sh
+++ octave/octave-script.sh
@@ -1,8 +1,12 @@
-if [ ! -f src/?akefile ]
-then
+if [ ! -f src/?akefile ]; then
     compilation_command=""
     run_command="octave-cli -H -W -q ${src_file}"
 fi
 
 export OPENBLAS_NUM_THREADS=1
+
+monitor_max_ms=1048576000 #1G
+# certas funcionalidades do octave necessitam de muita memória, então 256MB não é o suficiente nesse caso.
+# No caso especifico que eu testei, observei que 900MB era o suficiente, porém coloque 1G aqui para dar uma margem
+
 run_tests "$run_command"
