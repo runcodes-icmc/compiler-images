112bec9d9fd0fea53de057b669ef1515b1ce5037
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-03-10 19:44:48 -0300

    fix: some bash fixes

--- base/base-script.sh
+++ base/base-script.sh
@@ -18,7 +18,7 @@ monitor_max_ms=268435456 #256MB
 compilation_timeout=10
 src_file=''
 make_file=false
-[[ -e ${container_config_file} ]] && source ${container_config_file}
+[[ -e "${container_config_file}" ]] && source ${container_config_file}
 
 # Messages sent for notifying of current status
 compilation_start_msg='compilation.start'
@@ -31,33 +31,33 @@ compile() {
     pre_compilation_command=$2
     post_compilation_command=$3
 
-    echo ${compilation_start_msg}
+    echo "${compilation_start_msg}"
 
     # Save working dir to return to it later
-    wd=$(pwd)
+    wd="$(pwd)"
     cd src || exit
 
-    [ -z "${pre_compilation_command}" ] || ${pre_compilation_command}
+    [ -z "${pre_compilation_command}" ] || eval "${pre_compilation_command}"
 
-    ${monitor_bin} -f ${monitor_max_fs} \
-        -m ${monitor_max_ms} \
-        -o ../${compilation_output} \
-        -e ../${compilation_error} \
+    "${monitor_bin}" -f "${monitor_max_fs}" \
+        -m "${monitor_max_ms}" \
+        -o "../${compilation_output}" \
+        -e "../${compilation_error}" \
         -c "timeout --signal=SIGKILL ${compilation_timeout} ${compilation_command}" \
-        >../${compilation_monitor_output} \
-        2>../${compilation_monitor_error}
+        >"../${compilation_monitor_output}" \
+        2>"../${compilation_monitor_error}"
 
-    [ -z "${post_compilation_command}" ] || ${post_compilation_command}
+    [ -z "${post_compilation_command}" ] || eval "${post_compilation_command}"
 
     # Back to original working dir
     cd "$wd" || exit
 
-    cp ${compilation_monitor_output} ${outputfiles_dir}
-    cp ${compilation_monitor_error} ${outputfiles_dir}
-    cp ${compilation_output} ${outputfiles_dir}
-    cp ${compilation_error} ${outputfiles_dir}
+    cp "${compilation_monitor_output}" "${outputfiles_dir}"
+    cp "${compilation_monitor_error}" "${outputfiles_dir}"
+    cp "${compilation_output}" "${outputfiles_dir}"
+    cp "${compilation_error}" "${outputfiles_dir}"
 
-    echo ${compilation_done_msg}
+    echo "${compilation_done_msg}"
 }
 
 run_tests() {
@@ -65,26 +65,26 @@ run_tests() {
     pre_run_command=$2
     post_run_command=$3
 
-    echo ${run_start_msg}
+    echo "${run_start_msg}"
 
     # Save working dir to return to it later
-    wd=$(pwd)
+    wd="$(pwd)"
 
     while IFS= read -r -d '' test_dir; do
-        test_id=${test_dir#./test_}
+        test_id="${test_dir#./test_}"
         cp -r --update=none src/* "${test_dir}"
 
         cd "${test_dir}" || exit
 
         # This should have a value set in ${container_config_file}
-        var=t_${test_id}
-        [ -z "${!var}" ] && timeout=3 || timeout=${!var}
+        var="t_${test_id}"
+        [ -z "${!var}" ] && timeout=3 || timeout="${!var}"
 
-        [ -z "${pre_run_command}" ] || ${pre_run_command} "${test_id}"
+        [ -z "${pre_run_command}" ] || eval "${pre_run_command} ${test_id}"
 
         # Save output files outside of the test dir
-        ${monitor_bin} -f $monitor_max_fs \
-            -m $monitor_max_ms \
+        "${monitor_bin}" -f "$monitor_max_fs" \
+            -m "$monitor_max_ms" \
             -i "../${test_id}.in" \
             -o "../${test_id}.output" \
             -e "../${test_id}.error" \
@@ -92,7 +92,7 @@ run_tests() {
             >"../${test_id}.monitor_out" \
             2>"../${test_id}.monitor_err"
 
-        [ -z "${post_run_command}" ] || ${post_run_command} "${test_id}"
+        [ -z "${post_run_command}" ] || eval "${post_run_command} ${test_id}"
 
         # Back to original working dir
         cd "$wd" || exit
@@ -103,19 +103,16 @@ run_tests() {
         cp "${test_id}.error" "${outputfiles_dir}"
     done < <(find . -maxdepth 1 -type d -name 'test_*' -print0)
 
-    echo ${run_done_msg}
+    echo "${run_done_msg}"
 }
 
 # Make sure we have our directory for output files
-mkdir -p ${outputfiles_dir}
+mkdir -p "${outputfiles_dir}"
 
 # Standard commands for a Makefile-based submission. This can change depending
 # on the particular needs of each language.
-for file in src/?akefile; do
-    if [ -f "$file" ]; then
-        make_file=true
-        compilation_command='make all'
-        run_command='make -s run'
-        break
-    fi
-done
+if ! compgen -G "src/?akefile" >/dev/null; then
+    make_file=true
+    compilation_command='make all'
+    run_command='make -s run'
+fi
