d9c9f1ec48c4b9f714e81216dd8640a6b240ebaf
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-03-13 12:14:21 -0300

    fix: add compilation error handling

--- dotnet/dotnet-script.sh
+++ dotnet/dotnet-script.sh
@@ -1,6 +1,7 @@
 if ! compgen -G "src/?akefile" >/dev/null; then
     pre_compilation_command="dotnet new console -o . --force &>/dev/null && rm Program.cs"
     compilation_command="dotnet build -c Release"
+    post_compilation_command="grep -q \"FAILED\" ../${compilation_output} && cp ../${compilation_output} ../${compilation_error}"
     run_command="./bin/Release/net10.0/src"
 fi
 
@@ -8,7 +9,7 @@ monitor_max_fs=2684354560000
 monitor_max_ms=7516192768
 compilation_timeout=60
 
-compile "${compilation_command}" "${pre_compilation_command}"
+compile "${compilation_command}" "${pre_compilation_command}" "${post_compilation_command}"
 
 # Bail if there are any compilation errors
 if [ -s "${compilation_error}" ]; then
